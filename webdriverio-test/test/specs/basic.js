b6f623a31661ed382d38b42c9ad6cae7dc4d99ef
Anoop <simon.anoopsimon.anoop@gmail.com>
2020-01-02 12:01:59 +1100

    added one more test case

--- webdriverio-test/test/specs/basic.js
+++ webdriverio-test/test/specs/basic.js
@@ -1,11 +1,20 @@
 const assert = require('assert')
 
-describe('webdriver.io page', () => {
-    it('should have the right title', async () => {
+describe('WebdriverIO-HomepageTest', () => {
+    it('Verify valid title', async () => {
         //allureReporter.addFeature('Feature')
         await browser.url('https://webdriver.io')
         const title = await browser.getTitle()
         assert.strictEqual(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js')
-    })
+    })    
+})
+
+describe('WebdriverIO-HomepageTest1', () => {
+    it('Verify valid title', async () => {
+        //allureReporter.addFeature('Feature')
+        await browser.url('https://webdriver.io')
+        const title = await browser.getTitle()
+        assert.strictEqual(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js')
+    })    
 })
 
